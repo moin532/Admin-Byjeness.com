97ee6f7f3724d6debeedaf1de1dc4957d2d23e62
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2024-02-21 19:46:43 +0530

    Add files via upload

--- modules/MainRouter.js
+++ modules/MainRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Login, getProducts, getAdminOrders, getAdminUsers, getOrderdetails, getAdminSeller,  DeleteProduct, adminBuyer, LOaduser } = require('./Controller/UserController');
+const { Login, getProducts, getAdminOrders, getAdminUsers, getOrderdetails, getAdminSeller,  DeleteProduct, adminBuyer, LOaduser, ProductDetails } = require('./Controller/UserController');
 const router = express.Router();
 const { authenticateToken }  = require('./AuthMiddleware');
 
@@ -10,11 +10,12 @@ router.route('/me').get(LOaduser);
 
 //Product Admins --Route get
 router.route('/admin/products').get(authenticateToken,getProducts);
-router.route('/admin/Orders').get(getAdminOrders);;
-router.route('/admin/users').get(getAdminUsers);;
-router.route('/admin/Orders/Details').get(getOrderdetails);;
-router.route('/admin/seller').get(getAdminSeller);;
-router.route('/admin/buyer').get(adminBuyer);;
+router.route('/admin/product/:id').get(authenticateToken,ProductDetails);
+router.route('/admin/Orders').get(authenticateToken,getAdminOrders);;
+router.route('/admin/users').get(authenticateToken,getAdminUsers);;
+router.route('/admin/Orders/Details').get(authenticateToken,getOrderdetails);;
+router.route('/admin/seller').get(authenticateToken,getAdminSeller);;
+router.route('/admin/buyer').get(authenticateToken,adminBuyer);;
 
 //Product Admins --Route delete
 router.route('/admin/product/:id').delete(DeleteProduct);
